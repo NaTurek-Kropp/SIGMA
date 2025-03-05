90d894f210347f4ea2953e570535c6c29b9e91d7
Kerutt <itsnehomer@gmail.com>
2025-03-05 10:46:27 +0100

    Backend-Main-Data

---
+++ Main/Data.py
@@ -0,0 +1,19 @@
+def GetData(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    
+    questions = []
+    for i in range(0, len(lines), 5):
+        question = lines[i].strip()
+        answersWithImages = [tuple(line.strip().split(' ')) for line in lines[i+1:i+5]]
+        questions.append((question, answersWithImages))
+    
+    return questions
+
+file_path = 'Data/pytania.txt'
+questions = GetData(file_path)
+
+for question, answersWithImages in questions:
+    # print(question)
+    for answer in answersWithImages:
+        print((answer))
