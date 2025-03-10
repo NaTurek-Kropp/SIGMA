18297e8824c8249ca027dbca28a708990bc58320
Kerutt <itsnehomer@gmail.com>
2025-03-10 09:09:42 +0100

    Update Data.py

--- Main/Data.py
+++ Main/Data.py
@@ -1,25 +1,47 @@
-def GetQuestionsData(file_path):
+def GetQuestionsDataFixed(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
     
+    ALL = []
+    #QUESTIONS
     questions = []
     for i in range(0, len(lines), 5):
-        question = lines[i].strip()
-        answersWithImages = [tuple(line.strip().split(' ')) for line in lines[i+1:i+5]]
-        questions.append((question, answersWithImages))
-    
-    return questions
+        questionExh = lines[i].strip().split(' ')
+        question = []
+        for e in questionExh:
+            if e.startswith('http://') or e.startswith('https://'):
+                questions.append([" ".join(question), e])
+            else:
+                question.append(e)
+        #ANSWERS
+        answers = []
+        for line in lines[i+1:i+5]:
+            answerExh = line.strip().split(' ')
+            answer = []
+            for e in answerExh:
+                if e.startswith('http://') or e.startswith('https://'):
+                    answers.append([" ".join(answer), e])
+                    answer.clear()
+                elif e == answerExh[len(answerExh)-1]:
+                    answer.append(e)
+                    answers.append([" ".join(answer), ""])
+                    answer.clear()
+                else:
+                    answer.append(e)
+        
+        ALL.append(answers)
+    return questions, ALL
+
+
 
 file_path = 'Data/pytania.txt'
-questions = GetQuestionsData(file_path)
+questions = GetQuestionsDataFixed(file_path)
 
-for question, answersWithImages in questions:
-    for answer in answersWithImages:
-        print((answer))
+#print(questions[1][1])
 
 class Answers:
     def __init__(self):
         self.answersTable = []
 
-    def AppendAnswer(self, answer):
+    def AppendAnswer(self,answer):
         self.answersTable.append(answer)
